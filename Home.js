a26a7d0dd3edd7bae820a4fb76f75c61cb7d79f7
EarthStrix_Dev <78458755+EarthStrixDEV@users.noreply.github.com>
2022-02-21 17:58:53 +0700

    Add files via upload

--- Home.js
+++ Home.js
@@ -4,6 +4,7 @@ var element_nav = {
   a: document.querySelectorAll(".nav-header"),
   footer: document.querySelector(".footer-data"),
   footer_font: document.querySelectorAll(".footer-data-credit"),
+  footer_font_h3: document.querySelector('h3'),
   h1: document.getElementById("amz1_name"),
   h1_background_div: document.querySelector(".header-con2"),
   h1_background_span: document.querySelector(".header-name-con2"),
@@ -35,13 +36,20 @@ function theme() {
     footer_style: element_nav.footer.style.backgroundColor = "white",
     h1_name: element_nav.h1.style.color = 'black',
     h1_background_div: element_nav.h1_background_div.style.backgroundColor = 'white',
-    h1_background_span: element_nav.h1_background_span.style.backgroundColor = 'white'
+    h1_background_span: element_nav.h1_background_span.style.backgroundColor = 'white',
+    footer_h3: element_nav.footer_font_h3.style.color = 'black'
   };
 
   for (let j = 0; j < element_nav.footer_font.length; j++) {
     var element_footer_font_control = element_nav.footer_font[j];
     element_footer_font_control.style.color = "black";
   }
+
   console.log(element_footer_font_control);
   console.log(element);
+}
+
+function welcome()
+{
+  alert("Welcome to AMz_1🎮🚀");
 }
