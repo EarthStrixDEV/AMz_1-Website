c9dc36003f508155a99d4cbce07030d9895a9601
warapon6430300862 <warapon.jitsook@gmail.com>
2022-05-20 17:20:31 +0700

    Update 20/5/2022

--- src/Route/router.js
+++ src/Route/router.js
@@ -3,8 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('Home');
-})
+    res.render('welcome');
+});
+
+router.get('/home', (req, res) => {
+    res.render('home');
+    let para_id = req.params.id;
+    console.log(para_id);
+});
 
 router.get('/member', (req, res) => {
     res.render('Member');
@@ -12,6 +18,10 @@ router.get('/member', (req, res) => {
 
 router.get('/login', (req, res) => {
     res.render('login');
+})
+
+router.get('/register', (req, res) => {
+    res.render('register');
 })
 
 router.get('/home', (req, res) => {
---
+++ src/public/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#btn-login').click(function () {
+        $('#login-form').submit();
+    });
+
+    $('#btn-register').click(function () {
+        $('#register-form').submit();
+    });
+});
